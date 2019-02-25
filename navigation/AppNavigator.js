5334d883b48609f1d414ed2ed260d90ff973b807
Rahul <rahul.excel2011@gmail.com>
2019-02-25 15:37:39 +0530

    Added event page

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -3,9 +3,11 @@ import { createAppContainer, createStackNavigator } from 'react-navigation';
 import HomeScreen from '../screens/HomeScreen';
 import SetupScreen from '../screens/setup';
 import PermissionScreen from '../screens/permissionScreen';
+import EventScreen from '../screens/Events';
 
 export default createAppContainer(createStackNavigator({
-  Home: HomeScreen,
-  setup: SetupScreen,
-  permission: PermissionScreen,
+  Event: EventScreen,
+  // Home: HomeScreen,
+  // setup: SetupScreen,
+  // permission: PermissionScreen,
 })); 
