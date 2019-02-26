79aca8b6ffe35f23936e23b2aa07d01981fc06d7
Rahul <rahul.excel2011@gmail.com>
2019-02-26 12:03:20 +0530

    Notification Screen

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -4,8 +4,10 @@ import HomeScreen from '../screens/HomeScreen';
 import SetupScreen from '../screens/setup';
 import PermissionScreen from '../screens/permissionScreen';
 import EventScreen from '../screens/Events';
+import Notifications from '../screens/Notification';
 
 export default createAppContainer(createStackNavigator({
+  Notification: Notifications,
   Event: EventScreen,
   // Home: HomeScreen,
   // setup: SetupScreen,
