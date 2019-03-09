202ca1c18320d46c6a0fddaca3911109703a1b66
Rahul <rahul.excel2011@gmail.com>
2019-03-09 16:12:31 +0530

    fixed add to wishlist or remove from wishlist from events page

--- redux/addLikeOnEvent/reducer.js
+++ redux/addLikeOnEvent/reducer.js
@@ -1,34 +1,42 @@
 import { handleActions } from "redux-actions";
 import update from "immutability-helper";
-import * as constants from '../constant';
+import * as constants from "../constant";
 
 const initialState = {
-   isLoading: false,
-   isError: false,
-   isSuccess: false,
-   message: ""
+  isLoading: false,
+  isError: false,
+  isSuccess: false,
+  message: ""
 };
 
-const postAddLikeRequest = (state, action) =>{
-    return(
- update(state, {
-     isLoading: { $set: false },
-     isError: { $set: false },
-     message: { $set: "RequestSucccessfull" }
- }))};
+const postAddLikeRequest = (state, action) => {
+  return update(state, {
+    isLoading: { $set: true },
+    isError: { $set: false },
+    isSuccess: { $set: false },
+    message: { $set: "RequestSucccessfull" }
+  });
+};
 
 const postAddLikeSuccess = (state, action) =>
- update(state, {
-     isLoading: { $set: false },
-     isError: { $set: false },
-     isSuccess: { $set: true },
-     status: { $set: action.payload }
- });
+  update(state, {
+    isLoading: { $set: false },
+    isError: { $set: false },
+    isSuccess: { $set: true },
+    status: { $set: action.payload }
+  });
+
+const postAddLikeError = (state, action) =>
+  update(state, {
+    isLoading: { $set: false },
+    isError: { $set: true },
+    isSuccess: { $set: false }
+  });
 
 export default handleActions(
- {
-   [constants.POST_EVENT_LIKE_REQUEST]: postAddLikeRequest,
-   [constants.POST_EVENT_LIKE_SUCCESS]: postAddLikeSuccess
- },
- initialState
+  {
+    [constants.POST_EVENT_LIKE_REQUEST]: postAddLikeRequest,
+    [constants.POST_EVENT_LIKE_SUCCESS]: postAddLikeSuccess
+  },
+  initialState
 );
